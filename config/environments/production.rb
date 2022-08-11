a8818b9fe92464af1f685701aa2ab4cc0b00fc55
oumaima <lakhalouma6@gmail.com>
2022-08-11 11:44:40 +0100

    fixes9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,9 +17,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.assets.js_compressor = Uglifier.new(:harmony => true)
-
-  config.assets.initialize_on_precompile = false
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
