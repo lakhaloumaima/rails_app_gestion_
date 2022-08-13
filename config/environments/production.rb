df1b67178b200078a6348fc01c874f2f9005fa6e
oumaima <lakhalouma6@gmail.com>
2022-08-13 18:57:39 +0100

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000' #replace with your own url
+  host = 'https://gestionback.herokuapp.com' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
